16e088dcf86fc790b26e3c44d0d5757fb4902111
John Leamy <jwleamy@gmail.com>
2022-12-06 13:19:43 -0800

    on click, make api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,20 @@
 //    that store this data, and append them to main page when hitting 'submit' button.
 
 // 4. make everything look pretty with css adjustments and possible icons
-var sbar = document.getElementById('searchcity').value
-var userinput = $('input').val()
+var sbar = $('.form-control')
 var searchbutton = $('#basic-addon1')
+var WeatherAPI = '599f458e6722253dc1ab98813d04b95b';
+var city = ''
 
 searchbutton.on('click', function(event){
     event.preventDefault()
-    console.log(sbar)
-})
+    console.log($('.form-control').val())
+    city = $('.form-control').val()
 
-//variable that stores my personal API key
-var WeatherAPI = '599f458e6722253dc1ab98813d04b95b';
-var city = 'london';
+    var apicall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WeatherAPI;
 
-
-var apicall = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WeatherAPI;
-
-console.log(apicall)
+    console.log(city)
+    console.log(apicall)
 
 fetch(apicall)
     .then(function (response) {
@@ -33,3 +30,6 @@ fetch(apicall)
     .then(function (data) {
         console.log(data)
     })
+})
+
+//variable that stores my personal API key
